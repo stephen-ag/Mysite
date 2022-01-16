0b4b6d94f42ce86baec12c0152bf8994b261fb1c
stephen-ag <72654271+stephen-ag@users.noreply.github.com>
2022-01-16 13:08:15 +0530

    updated html page codes

--- app1.py
+++ app1.py
@@ -1,6 +1,17 @@
 import json
-from flask import Flask,render_template, request, jsonify, url_for,Response, redirect
+from flask import Flask,render_template, request, jsonify, url_for,Response, redirect,flash
 from flask_cors import cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
+from wtforms.widgets import TextArea
+from flask_migrate import Migrate
+from sqlite3 import dbapi2 as sqlite
+
+
+
 #import matplotlib.pyplot as plt
 #import xlwt
 # need to install xlrd and openpyxl to use read_excel function in pandas
@@ -11,17 +22,100 @@ import os
 import csv
 import shutil
 app = Flask(__name__)
+# add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# secret key
+app.config['SECRET_KEY'] = "my super secret key that no one"
+#initialise database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# create model for db
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique = True)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+    posts = db.relationship('Posts', backref = 'author', lazy = True)
+    # create a string
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}','{self.date_added}')"
+#CREATE a Blog post model
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+#    names = db.Column(db.String(200), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+# CREATE FORM CLASS
+class UserForm(FlaskForm):
+    name = StringField("Name", validators= [DataRequired()])
+    email = StringField("Email", validators= [DataRequired()])
+    submit = SubmitField("Submit")
+
+class BlogForm(FlaskForm):
+    names = StringField("Name", validators= [DataRequired()])
+    title = StringField("Title", validators=[DataRequired()])
+
+    content = StringField("Content", validators=[DataRequired()], widget= TextArea())
+
+    submit = SubmitField("Submit")
+
+@app.route('/add_user', methods = ['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email = form.email.data).first()
+        if user is None:
+            user= Users(name=form.name.data,email = form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfully")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html', form=form, name = name, our_users = our_users)
+
+##add blog POST MODEL######
+
+
+## add post form ###
+@app.route('/add_blog', methods = ['GET', 'POST'])
+def add_blog():
+    names = None
+    form = BlogForm()
+    if form.validate_on_submit():
+       # poster = current_user.id
+        post = Posts(title= form.title.data, content = form.content.data)
+        #clearing the form ##
+        form.title.data = ''
+        form.content.data = ''
+        form.names.data = ''
+
+        # add data to database #
+        db.session.add(post)
+        db.session.commit()
+        flash("Blog post submitted successfully ")
+
+    return render_template('add_blog.html', form=form)
 @app.route('/')
 @app.route('/home')
 @cross_origin()
 def home_page():
 
-    return render_template('index2.html')
+    return render_template('index3.html')
 
-@app.route('/about')
+@app.route('/blog')
 def about():
     title= "About time point screening"
-    return render_template('about.html', title=title)
+    return render_template('blog.html', title=title)
 
 @app.route('/clear_data',methods=['GET', 'POST'])
 def clear_data():
